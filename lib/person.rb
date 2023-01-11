a1165b464c05d9afe4a0c469d09357f24421329d
Andrew Owens <andrrowens@gmail.com>
2023-01-11 11:52:55 -0700

    commit tree ea00ad59ee442bf9984eb64c280cb2fbb6304b77

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,39 @@
 # your code goes here
+require "pry"
+class Person
+
+    attr_reader :name, :happiness
+    attr_accessor :bank_account
+
+    # attr_accessor :balance
+    # attr_reader :name, balance
+    # attr_writer :
+
+    def initialize(name)
+        @name = name 
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+
+    def happiness=(value)
+        if value <= 10 && value >= 0
+            @happiness = value
+        elsif value > 10
+            @happiness = 10
+        else
+            @happiness = 0
+        end
+    end
+
+    def return_self
+        self
+    end
+
+    # def
+    # end
+end
+
+matt = Person.new("Matt Eva")
+ryan = Person.new("Ryan Sullivan")
+
